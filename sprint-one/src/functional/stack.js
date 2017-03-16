a525d551af2bfb60bb56ddf03675afcb97d0a6bf
jun-Sogang <Sogang.jun.developer@gmail.com>
2017-03-16 09:31:17 +0900

    functional

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,30 @@
-var Stack = function() {
-  var someInstance = {};
+const Stack = function Stack() {
+  const someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
+  storage.size = 0;
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function push(value) {
+    storage[storage.size] = value;
+    storage.size += 1;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function pop() {
+    if (storage.size > 0) {
+      storage.size -= 1;
+      const poped = storage[storage.size];
+      storage[storage.size] = undefined;
+      return poped;
+    } else {
+      console.log("Error, can't pop");
+      return -1;
+    }
   };
 
-  someInstance.size = function() {
+  someInstance.size = function size() {
+    return storage.size;
   };
 
   return someInstance;
