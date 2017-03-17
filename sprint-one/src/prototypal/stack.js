fe2370261564bc64882fa6a6b8cbdfdb2588d1b6
jun-Sogang <Sogang.jun.developer@gmail.com>
2017-03-17 13:27:45 +0900

    prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
-var Stack = function() {
+const Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
-
-
+const stackMethods = {
+  storage : {},
+  key : 0,
+  push : function push(value) {
+     this.storage[this.key++] = value;
+  },
+  pop : function pop() {
+    if (this.key > 0) {
+      this.key -= 1;
+      const poped = this.storage[this.key];
+      this.storage[this.key] = undefined;
+      return poped;
+    } else {
+      console.log("Can't pop");
+    }
+  },
+  size : function size() {
+    return this.key;
+  },
+};
